fe5f07b7ab60de7aec38ad06f42386b0f73f090c
Ovidiu Grebenisan <ovidiu.grebenisan@iwelcome.com>
2022-03-08 19:21:46 +0200

    fix: corrected login route refactor: removed unused code fix: started the app with a production ready server

--- app/app.py
+++ app/app.py
@@ -2,17 +2,12 @@
 
 import os
 from flask import Flask, redirect
-from flask_jwt import JWT, jwt_required, current_identity
-from werkzeug.security import safe_str_cmp
+from waitress import serve
 
 app = Flask(__name__)
 
 try:
-    ISSUER_URL = os.environ["ISSUER_URL"]
     AUTH_URL = os.environ["AUTH_URL"]
-    TOKEN_URL = os.environ["TOKEN_URL"]
-    USERINFO_URL = os.environ["USERINFO_URL"]
-    JWKS_URL = os.environ["JWKS_URL"]
 except Exception as ex:
     raise
 
@@ -20,7 +15,7 @@ except Exception as ex:
 def index():
     return 'Hello world!'
 
-@app.route("/loogin")
+@app.route("/login")
 def login():
     return redirect(AUTH_URL, code=302)
 
@@ -29,4 +24,4 @@ def custom_callback():
     return "Logged in!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    serve(app, host='0.0.0.0', port=8000)
